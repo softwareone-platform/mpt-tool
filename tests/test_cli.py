20e672a8bef2992c0aa4e004e4d3b5b44f5f41d3
Susana Vázquez <3016283+svazquezco@users.noreply.github.com>
2026-01-08 19:52:20 +0100

    feat: add init command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -22,3 +22,53 @@ def test_help(runner):
     assert result.exit_code == 0
     assert "MPT CLI - Migration tool for extensions." in result.output
     assert "migrate" in result.output
+
+
+def test_init_command(monkeypatch, tmp_path, runner):
+    settings_file = tmp_path / "settings.py"
+    settings_file.write_text("FAKE_SETTING = True")
+    monkeypatch.chdir(tmp_path)
+
+    result = runner.invoke(app, ["init"])
+
+    assert result.exit_code == 0
+    migration_dir = tmp_path / "migrations"
+    assert migration_dir.exists()
+    state_file = tmp_path / ".migrations-state.json"
+    assert state_file.exists()
+    assert state_file.read_text() == "{}\n"
+
+
+def test_init_command_requires_setting(monkeypatch, tmp_path, runner):
+    monkeypatch.chdir(tmp_path)
+
+    result = runner.invoke(app, ["init"])
+
+    assert result.exit_code == 1
+    assert result.output == "Settings file does not exist.\n"
+
+
+def test_init_command_migrations_folder_exists(monkeypatch, tmp_path, runner):
+    settings_file = tmp_path / "settings.py"
+    settings_file.write_text("FAKE_SETTING = True")
+    migration_dir = tmp_path / "migrations"
+    migration_dir.mkdir()
+    monkeypatch.chdir(tmp_path)
+
+    result = runner.invoke(app, ["init"])
+
+    assert result.exit_code == 1
+    assert result.output == "Migrations folder already exists.\n"
+
+
+def test_init_command_state_file_exists(monkeypatch, tmp_path, runner):
+    settings_file = tmp_path / "settings.py"
+    settings_file.write_text("FAKE_SETTING = True")
+    state_file = tmp_path / ".migrations-state.json"
+    state_file.touch()
+    monkeypatch.chdir(tmp_path)
+
+    result = runner.invoke(app, ["init"])
+
+    assert result.exit_code == 1
+    assert result.output == "State file already exists.\n"
